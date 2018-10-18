e6e0470871cce4dcc62d895462a90370c4369ddd
Michael Soares <mikey.soares@gmail.com>
2018-10-18 14:37:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,6 +43,6 @@ function removeElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  array.splice(2,1)
+[...array.slice(0,2), ...array.slice(3)]
   return array
 }
